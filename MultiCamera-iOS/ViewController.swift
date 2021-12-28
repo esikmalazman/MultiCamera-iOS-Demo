e2eed2f8d5bb31f145e6cc4979dec559e6e5b0af
Ikmal Azman <opikmalazman@gmail.com>
2021-12-28 15:47:26 +0800

    Initial Commit

---
+++ MultiCamera-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MultiCamera-iOS
+//
+//  Created by Ikmal Azman on 28/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
